44927b6d8d19eebb46ed95c5a131409f20144434
Arman170616 <armanice09@gmail.com>
2023-05-09 01:42:31 +0600

    static_root & store app

--- project/settings.py
+++ project/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
+    'store',
     'crispy_forms',
     'crispy_bootstrap5',
 ]
---
+++ store/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from store.views import home
+
+
+urlpatterns = [
+    path('', home, name='index'),
+
+
+]
